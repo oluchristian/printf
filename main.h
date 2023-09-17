b7fd660aa8098539dfddd69786a5c5636930b41f
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2023-09-17 09:06:46 -0700

    added printf file

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdlib.h>
+#include <stdarg.h>
 #include <unistd.h>
+int _printf(char *, ...);
+int _putchar(char c);
 #endif
---
+++ printf.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _printf - prints values to stdout
+ *
+ * 
+ */
+
+int _printf(char *count, ...)
+{
+	int i;
+	va_list args;
+	va_start(args, count);
+
+	for(i = 0; i < count; i++)
+	{
+
+	}
+}
