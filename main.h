8881c9116d0f6ab074a3ac7377a80d2fbfb68b26
leumars-o <samex150@gmail.com>
2023-09-20 04:40:05 +0100

    functions modified to handle specifier "d" and "i".

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-#define BUFFER_SIZE 1024
+#define BUFF_SIZE 1024
 
 #include <stdarg.h>
 #include <unistd.h>
@@ -9,7 +9,8 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _puts(char *c);
-int _strlen (char *s);
+int _strlen(char *s);
 int prnt_func(va_list args, char s);
+int print_int(int n, int sign);
 
 #endif
--- prnt_func.c
+++ prnt_func.c
@@ -1,6 +1,44 @@
 #include "main.h"
 
 /**
+  * print_int - function prints out a signed integer
+  *
+  * @n: integer value
+  * @sign: bool that represents sign of the int
+  *
+  * Return: number of integers printed
+  */
+int print_int(int n, int sign)
+{
+	unsigned int neg = 0, d = 1, digit, num;
+
+	if (!sign)
+		num = (unsigned int)n;
+	else if (n < 0)
+	{
+		_putchar('-');
+		neg++;
+		num = n * (-1);
+	}
+	else
+		num = n;
+	digit = num / d;
+
+	while (digit > 9)
+	{
+		d *= 10;
+		digit = num / d;
+	}
+
+	while (d >= 1)
+	{
+		digit = ((num / d) % 10), d /= 10;
+		neg += _putchar('0' + digit);
+	}
+	return (neg);
+}
+
+/**
   * prnt_func - function that returns an identifier
   *
   * @s: paramenter of argument identifier
@@ -16,5 +54,8 @@ int prnt_func(va_list args, char s)
 		return (_puts(va_arg(args, char *)));
 	if (s == '%')
 		return (_putchar('%'));
+	if (s == 'd' || s == 'i')
+		return (print_int(va_arg(args, int), 1));
+
 	return (0);
 }
